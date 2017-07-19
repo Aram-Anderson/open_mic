2e8c0f7949f0003c7ba5dc367d15141571018c3c
Aram Anderson <aram.j.anderson@gmail.com>
2017-07-19 09:52:42 -0600

    Up through itteration 4 tested and working

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,25 @@
+require './lib/joke'
+
 class User
+
+  attr_accessor :jokes
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @jokes = []
   end
+
+  def learn(joke)
+    @jokes << joke
+  end
+
+  def tell(listener, joke)
+    listener.jokes << @jokes[joke]
+  end
+
+  def perform_routine_for(listener)
+    listener.jokes = @jokes
+  end
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -2,6 +2,8 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/user'
+require './lib/joke'
+require 'pry'
 
 class UserTest < Minitest::Test
   def test_it_exists
@@ -21,4 +23,48 @@ class UserTest < Minitest::Test
 
     assert_equal "Ali", ali.name
   end
+
+  def test_it_can_learn_a_joke
+    sal = User.new("Sal")
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal 0, sal.jokes.count
+
+    sal.learn(joke)
+
+    assert_equal 1, sal.jokes.count
+
+  end
+
+  def test_it_can_tell_a_joke
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    sal.learn(joke)
+
+    assert_equal 0, ali.jokes.count
+
+    sal.tell(ali, 0)
+
+    assert_equal 1, ali.jokes.count
+  end
+
+  def test_it_can_perform_a_routine
+    joke_1 = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    joke_2 = Joke.new({id: 2, question: "How do you keep a lion from charging?", answer: "Take away its credit cards."})
+
+    ilana = User.new("Ilana")
+    josh = User.new("Josh")
+
+    ilana.learn(joke_1)
+    ilana.learn(joke_2)
+
+    assert_equal 0, josh.jokes.count
+
+    ilana.perform_routine_for(josh)
+
+    assert_equal 2, josh.jokes.count
+  end
+
+
 end
